261a60d7615f867e36b27edd85e054e73740c91f
przyk <166012950+przyk@users.noreply.github.com>
2025-03-09 21:53:35 +0100

    Update filters.py

--- rangefilter/filters.py
+++ rangefilter/filters.py
@@ -199,7 +199,7 @@ class DateRangeFilter(BaseRangeFilter):
                     self.lookup_kwarg_gte,
                     forms.DateField(
                         label="",
-                        widget = AdminDateWidget(attrs={"placeholder": "{}{}".format(_('From '), _(self.field_path))})
+                        widget=AdminSplitDateTime(attrs={"placeholder": _("From date")}),
                         localize=True,
                         required=False,
                         initial=self.default_gte,
